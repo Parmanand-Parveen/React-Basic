c2a6c79e172076c67bf22ff08c01ab94f12e571c
Parmanand-Parveen <parmanandparveen@gmail.com>
2023-10-10 19:48:12 +0530

    Files✔️

---
+++ React basic/props project/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { Cardcontainer } from './Components/Cardcontainer'
+
+function App() {
+return(
+  <>
+   <Cardcontainer></Cardcontainer>
+  </>
+)
+}
+
+export default App
---
+++ React basic/props project/src/Components/Cardcontainer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Cards } from './Cards'
+
+export const Cardcontainer = () => {
+  
+  const username = [Parmanand,Rahul,Saba,Sabrin,Tannu]
+  const Dob= [2/10/2003,12/12/2000,9/3/2003,27/4/2004,4/8/2004]
+  return (
+<>
+  
+</>
+  )
+}
+
