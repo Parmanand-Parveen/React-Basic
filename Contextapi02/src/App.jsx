053590a28cbb5ffdb600bf1bde40c84a86d0adb4
Parmanand-Parveen <parmanandparveen@gmail.com>
2024-03-15 19:23:39 +0530

    Conetext Api 02

---
+++ Contextapi02/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState,useEffect } from 'react'
+import './App.css'
+import { ThemeProvider } from './Context/ThemeSwitcher'
+import Card from './Component/Card'
+import Themebtn from './Component/Themebtn'
+
+function App() {
+  const [themeMode, setThemeMode] = useState("light")
+
+  const lightTheme= ()=>{
+    return setThemeMode("light")
+  }
+
+  const darkTheme = ()=>{
+    return setThemeMode('dark')
+  }
+
+  useEffect(() => {
+    document.querySelector('html').classList.remove('light', 'dark')
+    document.querySelector('html').classList.add(themeMode)
+  }, [themeMode])
+  
+
+  return (
+    <>
+    <ThemeProvider value={{themeMode,darkTheme,lightTheme}}>
+    <div className="flex flex-wrap min-h-screen items-center">
+    <div className="w-full">
+        <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+          <Themebtn/>
+        </div>
+
+        <div className="w-full max-w-sm mx-auto">
+           <Card/>
+        </div>
+    </div>
+</div>
+    
+    </ThemeProvider>
+    </>
+  )
+}
+
+export default App
